ce635bbc39f02a7902dfd31d80f984e6766da7de
Viktor <vikmillion@gmail.com>
2022-03-18 15:28:33 +0300

    scss

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('work');
+const Car = class {
+  constructor(brand, color) {
+    this.brand = brand;
+    this.color = color;
+  }
+  start() {
+    console.log(`${this.brand} -start!`);
+  }
+  stop() {
+    console.log(`${this.brand} -stop!`);
+  }
+  static discount() {
+    console.log('Discount ');
+  }
+  set rating(value) {
+    this.score = value;
+  }
+  get rating() {
+    return this.score;
+  }
+};
+
+const tesla = new Car('Tesla', 'red');
+console.log(tesla);
+ 
